1155899c45a2c637e6551b46cc9e4dcfc1d9eee1
dingdingsong <dingsong1@xiaomi.com>
2023-03-03 16:31:56 +0800

    [new] test
    Signed-off-by: dingdingsong <1749905935@qq.com>

--- demo_nodes_cpp/src/parameters/even_parameters_node.cpp
+++ demo_nodes_cpp/src/parameters/even_parameters_node.cpp
@@ -21,71 +21,77 @@
 
 namespace demo_nodes_cpp
 {
-class EvenParameterNode : public rclcpp::Node
-{
-public:
-  DEMO_NODES_CPP_PUBLIC
-  explicit EvenParameterNode(rclcpp::NodeOptions options)
-  : Node("even_parameters_node", options.allow_undeclared_parameters(true))
+  class EvenParameterNode : public rclcpp::Node
   {
-    RCLCPP_INFO(get_logger(), "This example node shows a parameter callback that rejects");
-    RCLCPP_INFO(get_logger(), "all parameter updates except for those that set an even integer.");
-    RCLCPP_INFO(get_logger(), "Try running 'ros2 param set /even_parameters_node myint 2' to");
-    RCLCPP_INFO(get_logger(), "successfully set a parameter.");
+  public:
+    DEMO_NODES_CPP_PUBLIC
+    explicit EvenParameterNode(rclcpp::NodeOptions options)
+        : Node("even_parameters_node", options.allow_undeclared_parameters(true))
+    {
+      // *******************************dingsong_test****************************************
+      // *******************************dingsong_test****************************************
+      RCLCPP_INFO(get_logger(), "This example node shows a parameter callback that rejects");
+      RCLCPP_INFO(get_logger(), "all parameter updates except for those that set an even integer.");
+      RCLCPP_INFO(get_logger(), "Try running 'ros2 param set /even_parameters_node myint 2' to");
+      RCLCPP_INFO(get_logger(), "successfully set a parameter.");
 
-    // Declare a parameter change request callback
-    // This function will enforce that only setting even integer parameters is allowed
-    // any other change will be discarded
-    auto param_change_callback =
-      [this](std::vector<rclcpp::Parameter> parameters)
+      // Declare a parameter change request callback
+      // This function will enforce that only setting even integer parameters is allowed
+      // any other change will be discarded
+      auto param_change_callback =
+          [this](std::vector<rclcpp::Parameter> parameters)
       {
         auto result = rcl_interfaces::msg::SetParametersResult();
         result.successful = true;
-        for (auto parameter : parameters) {
+        for (auto parameter : parameters)
+        {
           rclcpp::ParameterType parameter_type = parameter.get_type();
-          if (rclcpp::ParameterType::PARAMETER_NOT_SET == parameter_type) {
+          if (rclcpp::ParameterType::PARAMETER_NOT_SET == parameter_type)
+          {
             RCLCPP_INFO(
-              this->get_logger(), "parameter '%s' deleted successfully",
-              parameter.get_name().c_str()
-            );
+                this->get_logger(), "parameter '%s' deleted successfully",
+                parameter.get_name().c_str());
             result.successful &= true;
-          } else if (rclcpp::ParameterType::PARAMETER_INTEGER == parameter_type) {
-            if (parameter.as_int() % 2 != 0) {
+          }
+          else if (rclcpp::ParameterType::PARAMETER_INTEGER == parameter_type)
+          {
+            if (parameter.as_int() % 2 != 0)
+            {
               RCLCPP_INFO_STREAM(
-                this->get_logger(),
-                "Requested value '" << parameter.as_int() << "' for parameter '" <<
-                  parameter.get_name() << "' is not an even number: rejecting change..."
-              );
+                  this->get_logger(),
+                  "Requested value '" << parameter.as_int() << "' for parameter '" << parameter.get_name() << "' is not an even number: rejecting change...");
               result.reason = "only even integers can be set";
               result.successful = false;
-            } else {
+            }
+            else
+            {
               RCLCPP_INFO(
-                this->get_logger(),
-                "parameter '%s' has changed and is now: %s",
-                parameter.get_name().c_str(),
-                parameter.value_to_string().c_str()
-              );
+                  this->get_logger(),
+                  "parameter '%s' has changed and is now: %s",
+                  parameter.get_name().c_str(),
+                  parameter.value_to_string().c_str());
               result.successful &= true;
             }
-          } else {
+          }
+          else
+          {
             RCLCPP_INFO(
-              this->get_logger(), "only integer parameters can be set\n"
-              "requested value for parameter '%s' is not an even number, rejecting change...",
-              parameter.get_name().c_str()
-            );
+                this->get_logger(), "only integer parameters can be set\n"
+                                    "requested value for parameter '%s' is not an even number, rejecting change...",
+                parameter.get_name().c_str());
             result.reason = "only integer parameters can be set";
             result.successful = false;
           }
         }
         return result;
       };
-    // callback_handler needs to be alive to keep the callback functional
-    callback_handler = this->add_on_set_parameters_callback(param_change_callback);
-  }
+      // callback_handler needs to be alive to keep the callback functional
+      callback_handler = this->add_on_set_parameters_callback(param_change_callback);
+    }
 
-  OnSetParametersCallbackHandle::SharedPtr callback_handler;
-};
+    OnSetParametersCallbackHandle::SharedPtr callback_handler;
+  };
 
-}  // namespace demo_nodes_cpp
+} // namespace demo_nodes_cpp
 
 RCLCPP_COMPONENTS_REGISTER_NODE(demo_nodes_cpp::EvenParameterNode)
